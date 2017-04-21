69774e1cdec2ec4c5f4f29c5eb37567e9efad7ad
Juan Gabriel <juangabriel@pixeloak.com>
2017-04-20 23:48:50 -0500

    Escaped single quotes

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ class App extends Component {
       <article className="App">
         <header>
           <h2 className="page-title">Your Email Organized</h2>
-          <h4>You can recategorize your senders or leave them in your Inbox by unchecking the box next to the sender's name.</h4>
+          <h4>You can recategorize your senders or leave them in your Inbox by unchecking the box next to the sender&#39;s name.</h4>
         </header>
         <section>
           <div className="filters">
